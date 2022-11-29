18dc91cd468fcff748afe886c84eb12c5e2c5d49
Michalina <michalina.oniszczuk@gmail.com>
2022-11-29 17:08:49 +0100

    project layout changes

--- src/pages/projects/components/Project.jsx
+++ src/pages/projects/components/Project.jsx
@@ -2,9 +2,9 @@
 export function Project(props) {
   return (
     <div className="single_project">
-      <h3>{props.name}</h3>
       <a href={props.solution_link} target="_blank" className="project_img"><img src={props.img_link} alt={props.img_alt}></img></a>
       <div className="text_content">
+        <h3>{props.name}</h3>
         <section className="description">{props.description}</section>
         <div className="button_container">
           {props.buttons}
