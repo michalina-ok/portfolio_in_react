907bed3c5f7500880fb37d4007c9df181dd0d84d
Michalina <michalina.oniszczuk@gmail.com>
2022-11-29 16:50:43 +0100

    improved project linking

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -9,6 +9,7 @@ function ProjectsPage() {
       <Project
         name="Gerner Jahncke"
         description="Gerner Jahncke was a final 2nd semester project done collaboratively in a 4-people team. We conducted user research and created a website design and social media strategy for an independent ceramics studio based in Bornholm. The website was also developed using HTML, CSS and JavaScript."
+        solution_link="http://gareth.dk/portfolio/Assets/Projects/gernerJahncke/index.html"
         img_link="/gerner_jahncke.webp"
         img_alt="Screenshot of a website. The header says 'Gerner Jahncke'. The background picture presents vases and glasses on a table. There are four categories a user can choose - our products, our process, contact anmd comissions."
         buttons={[
@@ -20,7 +21,8 @@ function ProjectsPage() {
         name="Hogwart assignment"
         description="Hogwart assignment was a final project of a 5 week long theme called 'Just JavaScript'. The goal of the assignment was to create a database of Hogwart students, display information about them and let the user perform some actions e.x.
       add students to the prefect team or to the inquisitorial squad. Not my proudest design but hey, I learnt a lot about JavaScript. "
-        img_link="/hogwart_assignment.webp"
+      solution_link="https://michalina-ok.github.io/hogwart_assignment/"  
+      img_link="/hogwart_assignment.webp"
         img_alt="Screenshot of a website. In the foreground there is a card containing data about a selected student: his name, picture and information about his status"
         buttons={[
         <Button text="Website" url="https://michalina-ok.github.io/hogwart_assignment/" />, 
@@ -29,6 +31,7 @@ function ProjectsPage() {
       <Project
         name="Alternative portfolio"
         description="Previous version of this page. Built before I have started learning React so it's based on vanilla JavaScript, HTML and Sass. Still minimal yet a bit more playful design including a geometrical header."
+        solution_link="https://michalina-oniszczuk-portfolio-js.netlify.app/"  
         img_link="/alternative_portfolio.webp"
         img_alt="Screenshot of a website. On one side there is a vertical header with three categories: projects, about, contact and a circle on top with them with 'Michalina Oniszczuk' written inside it. Centrally there is text saying: 'Hi. My name is Michalina and I design and develop stuff. I am currently looking for a 10-week internship within the field of creative development and product design' and a text saying 'See my projects'."
         buttons={[
--- src/pages/projects/components/Project.jsx
+++ src/pages/projects/components/Project.jsx
@@ -3,7 +3,7 @@ export function Project(props) {
   return (
     <div className="single_project">
       <h3>{props.name}</h3>
-      <a href=""><img src={props.img_link} alt={props.img_alt}></img></a>
+      <a href={props.solution_link} target="_blank" className="project_img"><img src={props.img_link} alt={props.img_alt}></img></a>
       <div className="text_content">
         <section className="description">{props.description}</section>
         <div className="button_container">
